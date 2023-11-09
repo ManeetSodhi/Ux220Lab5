263c8f5fb039eaf2703c250a7b93cd5f00dff812
Maneetsodhi <maneetsodhi03@gmail.com>
2023-11-08 20:48:42 -0500

    add center and date custom element

--- customelement.js
+++ customelement.js
@@ -5,3 +5,20 @@ class ManeetElement extends HTMLElement{
 }
 
 customElements.define("x-maneet", ManeetElement);
+
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>
+        `
+    }
+}
+
+customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-fullyear", YearElement);
